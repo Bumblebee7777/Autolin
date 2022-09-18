042f34b7c56e9f7bcb64507a0caa216178350d63
Esteban Gambaro <gambaroesteban@gmail.com>
2022-09-18 20:33:43 -0300

    card

--- app.js
+++ app.js
@@ -1,93 +1,120 @@
-alert("Bienvenidos a AUTOLIN")
+// alert("Bienvenidos a AUTOLIN")
 
 
 
-function registroUsuario() {
+// function registroUsuario() {
     
-   return prompt("Ingrese su nuevo usuario")
-}
+//    return prompt("Ingrese su nuevo usuario")
+// }
 
-registroUsuario();
+// registroUsuario();
 
-function registroContraseña() {
+// function registroContraseña() {
 
-   return prompt("Ingrese su nuevo contraseña")
-};
+//    return prompt("Ingrese su nuevo contraseña")
+// };
 
-registroContraseña();
+// registroContraseña();
 
-let usuarioNuevo = registroUsuario()
-let contraseñaNueva = registroContraseña()
+// let usuarioNuevo = registroUsuario()
+// let contraseñaNueva = registroContraseña()
 
-alert("Se registro correctamente");
+// alert("Se registro correctamente");
 
-function loguear() {
-   let usuarioRegistrado = prompt("INGRESE SU NOMBRE DE USUARIO")
-   let contraseñaRegistrada = prompt("INGRESE SU CONTRASEÑA")
+// function loguear() {
+//    let usuarioRegistrado = prompt("INGRESE SU NOMBRE DE USUARIO")
+//    let contraseñaRegistrada = prompt("INGRESE SU CONTRASEÑA")
 
-   while(usuarioRegistrado != usuarioNuevo && contraseñaRegistrada != contraseñaNueva){
-      alert("DATOS INCORRECTOS")
-      loguear()
-   }
-};
+//    while(usuarioRegistrado != usuarioNuevo && contraseñaRegistrada != contraseñaNueva){
+//       alert("DATOS INCORRECTOS")
+//       loguear()
+//    }
+// };
 
-loguear();
+// loguear();
 
-alert("Ha iniciado sesion correctamente, puedo seguir a elegir su auto")
+// alert("Ha iniciado sesion correctamente, puedo seguir a elegir su auto")
 
 class Auto{
-   constructor(modelo, precio){
+   constructor(imagen, modelo, precio){
+      this.imagen = imagen
       this.modelo = modelo
       this.precio = precio
    }
 }
-const trend = new Auto("Gol Trend", 200000)
-const sentra = new Auto("Nissan Sentra", 300000)
-const peugeot = new Auto("208gti", 500000)
-const cruze = new Auto("Chevrolet Cruze", 400000)
+const trend = new Auto("images/trend.jpg", "Gol Trend", 200000)
+const sentra = new Auto("images/sentra.jpg", "Nissan Sentra", 300000)
+const peugeot = new Auto("images/208.jpg", "208gti", 500000)
+const cruze = new Auto("images/cruze.jpg","Chevrolet Cruze", 400000)
+
+let arrayAutos = [trend, sentra, peugeot,cruze];
+
+let contenedorCard = document.getElementById("contenedor-card")
+arrayAutos.forEach(producto => {
+   let div = document.createElement("div")
+   div.innerHTML = ` <div class="card" style="width: 18rem;">
+   <img src="${producto.imagen}" class="card-img-top" alt="...">
+   <div class="card-body">
+     <h5 class="card-title">${producto.modelo}</h5>
+     <p class="card-text">${producto.precio}</p>
+     <a href="#" class="btn btn-primary" id="btn-compra">Agregar al carrito</a>
+   </div>
+ </div>`
+ contenedorCard.appendChild(div)
+}) 
+
+
 
 const carrito = []
 
-let auto = prompt("Seleccione su auto:  \n 1- Gol TREND $200000 \n 2- Nissan Sentra $300000 \n 3- Peugeot 208gti $500000  \n 4-Chevrolet CRUZE $400000 \n 5-Ingrese 'Terminar compra' para salir")
+let btnCompra = document.getElementById("btn-compra")
 
-function seleccionarAuto() {
+btnCompra.addEventListener("click", () => {
+   
+})
 
- while(auto != "Terminar compra"){
-     switch(auto){
-         case "1": 
-             alert("Usted a seleccionado Gol TREND")
-           carrito.push(trend)
-             break;
-          case"2":  
-             alert("Usted a seleccionado Nissan SENTRA")
-             carrito.push(sentra)
-             break;
-          case "3":
-             alert("Usted a seleccionado Peugeot 208")
-             carrito.push(peugeot)
+
+
+// let auto = prompt("Seleccione su auto:  \n 1- Gol TREND $200000 \n 2- Nissan Sentra $300000 \n 3- Peugeot 208gti $500000  \n 4-Chevrolet CRUZE $400000 \n 5-Ingrese 'Terminar compra' para salir")
+
+// function seleccionarAuto() {
+
+//  while(auto != "Terminar compra"){
+//      switch(auto){
+//          case "1": 
+//              alert("Usted a seleccionado Gol TREND")
+//            carrito.push(trend)
+//              break;
+//           case"2":  
+//              alert("Usted a seleccionado Nissan SENTRA")
+//              carrito.push(sentra)
+//              break;
+//           case "3":
+//              alert("Usted a seleccionado Peugeot 208")
+//              carrito.push(peugeot)
          
-             break;
-          case "4":
-             alert("seleccionado Chevrolet CRUZE")
-             carrito.push(cruze)
-             default:
+//              break;
+//           case "4":
+//              alert("seleccionado Chevrolet CRUZE")
+//              carrito.push(cruze)
+//              default:
                
-             alert("Opcion incorrecta")
+//              alert("Opcion incorrecta")
              
-             break;
-         }
- auto = prompt("Seleccione su auto:  \n 1- Gol TREND $200000 \n 2- Nissan Sentra $300000 \n 3- Peugeot 208gti $500000  \n 4-Chevrolet CRUZE $400000 \n 5-Ingrese 'Terminar compra' para salir")
+//              break;
+//          }
+//  auto = prompt("Seleccione su auto:  \n 1- Gol TREND $200000 \n 2- Nissan Sentra $300000 \n 3- Peugeot 208gti $500000  \n 4-Chevrolet CRUZE $400000 \n 5-Ingrese 'Terminar compra' para salir")
 
  
-     }
-}
+//      }
+// }
 
-seleccionarAuto();
+// seleccionarAuto();
 
 
-alert("Gracias por su compra, presione F12 para ver su carrito")
+// alert("Gracias por su compra, presione F12 para ver su carrito")
 
-carrito.forEach(producto => console.log(producto))
+// carrito.forEach(producto => console.log(producto))
 
-let precioTotal = carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)
-console.log("El precio total es de $" + precioTotal)  
+// let precioTotal = carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)
+// console.log("El precio total es de $" + precioTotal)  
