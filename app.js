921503ee7a32cfe8544d90a12666a2f91345e9d9
Esteban Gambaro <gambaroesteban@gmail.com>
2022-10-10 19:28:50 -0300

    carrito

--- app.js
+++ app.js
@@ -1,73 +1,39 @@
-// alert("Bienvenidos a AUTOLIN")
 
-
-
-// function registroUsuario() {
-    
-//    return prompt("Ingrese su nuevo usuario")
-// }
-
-// registroUsuario();
-
-// function registroContraseña() {
-
-//    return prompt("Ingrese su nuevo contraseña")
-// };
-
-// registroContraseña();
-
-// let usuarioNuevo = registroUsuario()
-// let contraseñaNueva = registroContraseña()
-
-// alert("Se registro correctamente");
-
-// function loguear() {
-//    let usuarioRegistrado = prompt("INGRESE SU NOMBRE DE USUARIO")
-//    let contraseñaRegistrada = prompt("INGRESE SU CONTRASEÑA")
-
-//    while(usuarioRegistrado != usuarioNuevo && contraseñaRegistrada != contraseñaNueva){
-//       alert("DATOS INCORRECTOS")
-//       loguear()
-//    }
-// };
-
-// loguear();
-
-// alert("Ha iniciado sesion correctamente, puedo seguir a elegir su auto")
 
 class Auto{
-   constructor(imagen, modelo, precio, id){
+   constructor(imagen, modelo, precio, id, cantidad){
       this.imagen = imagen
       this.modelo = modelo
       this.precio = precio
       this.id = id
+      this.cantidad = cantidad
    }
 }
 const carrito = []
 
 
-const trend = new Auto("images/trend.jpg", "Gol Trend", 200000,"1")
-const sentra = new Auto("images/sentra.jpg", "Nissan Sentra", 300000, "2")
-const peugeot = new Auto("images/208.jpg", "208gti", 500000, "3")
-const cruze = new Auto("images/cruze.jpg","Chevrolet Cruze", 400000, "4")
-const civic = new Auto("images/civic.jpg", "Honda Civic", 600000, "5" )
-const focus = new Auto("images/focus.jpg", "Ford Focus", 700000, "6")
-const corolla = new Auto("images/corolla.jpg", "Toyota Corolla", 800000, "7")
-const tiguan = new Auto("images/tiguan.jpg", "Volkswagen Tiguan", 900000, "8")
-const suran = new Auto("images/suran.jpg", "Volkswagen Suran", 1000000, "9")
-const tresmil = new Auto("images/3008.jpg", "Peugeot 3008", 1100000, "10")
+const trend = new Auto("images/trend.jpg", "Gol Trend", 200000,"1", 1)
+const sentra = new Auto("images/sentra.jpg", "Nissan Sentra", 300000, "2", 1)
+const peugeot = new Auto("images/208.jpg", "208gti", 500000, "3", 1)
+const cruze = new Auto("images/cruze.jpg","Chevrolet Cruze", 400000, "4", 1)
+const civic = new Auto("images/civic.jpg", "Honda Civic", 600000, "5", 1)
+const focus = new Auto("images/focus.jpg", "Ford Focus", 700000, "6", 1)
+const corolla = new Auto("images/corolla.jpg", "Toyota Corolla", 800000, "7", 1)
+const tiguan = new Auto("images/tiguan.jpg", "Volkswagen Tiguan", 900000, "8", 1)
+const suran = new Auto("images/suran.jpg", "Volkswagen Suran", 1000000, "9", 1)
+const tresmil = new Auto("images/3008.jpg", "Peugeot 3008", 1100000, "10", 1)
 
 let arrayAutos = [trend, sentra, peugeot, cruze, civic, focus, corolla, tiguan, suran, tresmil ];
 
 let contenedorCard = document.getElementById("contenedor-card")
 arrayAutos.forEach(producto => {
    let div = document.createElement("div")
-   div.innerHTML = ` <div class="card" style="width: 18rem;">
-   <img src="${producto.imagen}" class="card-img-top" alt="...">
+   div.innerHTML = ` <div class="card" >
+   <img src="${producto.imagen}" >
    <div class="card-body">
      <h5 class="card-title">${producto.modelo}</h5>
      <p class="card-text">${producto.precio}</p>
-     <button class="btn btn-primary" id="button${producto.id}">Agregar al carrito</button>
+     <button class="button" id="button${producto.id}">Agregar al carrito</button>
    </div>
  </div>`
  contenedorCard.appendChild(div)
@@ -104,86 +70,64 @@ if(localStorage.getItem("compras")){
    }
 }
 
-const mostrarCarrito = document.getElementById("carrito-compra")
+const mostrarCarrito = document.getElementById("products-container")
 
 function actualizarCarrito(){ 
    let acumuladorAuto = ""
    carrito.forEach(producto => {
-      acumuladorAuto += `<div class="card" style="width: 18rem;">
-      <img src="${producto.imagen}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${producto.modelo}</h5>
-        <p class="card-text">${producto.precio}</p>
-        <button onClick = eliminarAuto(${producto.id}) > X </button>
-        </div>
-      </div>`
+      acumuladorAuto += `<div class="products-cards">
+      <h4 class="text-principal">${producto.modelo}</h4>
+      <i class="text-principal">Precio: $${producto.precio}</i>
+      <i>Cantidad: ${producto.cantidad}</i>
+      <button onclick = eliminarProducto(${producto.id})>Eliminar</button>
+      </div>
+    <hr>`
    })
    mostrarCarrito.innerHTML = acumuladorAuto
-   
+   totalCarrito()
 }
 
+let modalCarrito = document.getElementById("carrito-conteiner") 
 
- const eliminarAuto = (id) => {
+
+let buttonCarritoOpen = document.getElementById("btn-carrito")
+
+buttonCarritoOpen.addEventListener("click", () => {
+   modalCarrito.className = "carrito-conteiner"
+})
+
+let buttonCarritoClose = document.getElementById("shop-close")
+
+buttonCarritoClose.addEventListener("click",() => {
+   modalCarrito.className = "none"
+})
+
+
+ const eliminarProducto = (id) => {
    const producto = carrito.find(producto => producto.id === id);
   carrito.splice(carrito.indexOf(producto), 1);
   actualizarCarrito()
  }
 
 
- 
-
-
-
-// let btnCompra = document.getElementById("btn-compra")
-
-// btnCompra.addEventListener("click", () => {
-   
-//    alert(`Agregaste ${producto.name}
-
-
-
-
-
-// let auto = prompt("Seleccione su auto:  \n 1- Gol TREND $200000 \n 2- Nissan Sentra $300000 \n 3- Peugeot 208gti $500000  \n 4-Chevrolet CRUZE $400000 \n 5-Ingrese 'Terminar compra' para salir")
-
-// function seleccionarAuto() {
-
-//  while(auto != "Terminar compra"){
-//      switch(auto){
-//          case "1": 
-//              alert("Usted a seleccionado Gol TREND")
-//            carrito.push(trend)
-//              break;
-//           case"2":  
-//              alert("Usted a seleccionado Nissan SENTRA")
-//              carrito.push(sentra)
-//              break;
-//           case "3":
-//              alert("Usted a seleccionado Peugeot 208")
-//              carrito.push(peugeot)
-         
-//              break;
-//           case "4":
-//              alert("seleccionado Chevrolet CRUZE")
-//              carrito.push(cruze)
-//              default:
-               
-//              alert("Opcion incorrecta")
-             
-//              break;
-//          }
-//  auto = prompt("Seleccione su auto:  \n 1- Gol TREND $200000 \n 2- Nissan Sentra $300000 \n 3- Peugeot 208gti $500000  \n 4-Chevrolet CRUZE $400000 \n 5-Ingrese 'Terminar compra' para salir")
 
  
-//      }
-// }
+ //Funcion para vaciar por completo el carrito
+ let vaciarCarrito = document.getElementById("delete-shop");
+ vaciarCarrito.addEventListener("click", () => {
+   carrito.splice(0, carrito.length);
+   actualizarCarrito();
+ });
+ 
+ //Calculo el total del carrito
+ const totalCompra = document.getElementById("total");
+ 
+ function totalCarrito() {
+   let total = 0;
+   carrito.forEach(producto => {
+     total += producto.precio * producto.cantidad;
+   });
+   totalCompra.innerHTML = total;
+ }
 
-// seleccionarAuto();
 
-
-// alert("Gracias por su compra, presione F12 para ver su carrito")
-
-// carrito.forEach(producto => console.log(producto))
-
-// let precioTotal = carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)
-// console.log("El precio total es de $" + precioTotal)
